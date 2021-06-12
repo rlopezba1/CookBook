04e607779f07b49941fafbd6868951d3de6d3b32
rlopezba1 <rubenlopezb@gmail.com>
2021-06-12 15:48:32 +0000

    fixing recipes grid on main page

--- app.py
+++ app.py
@@ -157,7 +157,7 @@ def get_categories():
     return render_template("categories.html", categories=categories)
 
 
-@app.route("/add_category", methods=["GET","POST"])
+@app.route("/add_category", methods=["GET", "POST"])
 def add_category():
     if request.method == "POST":
         category = {
